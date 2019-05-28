afb5d01b8102be9fc951a63ad0f84e6df56f4ed3
mosesdock <48386468+mosesdock@users.noreply.github.com>
2019-05-28 08:19:18 +0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -8,7 +8,7 @@ variable "AMIS" {
     default = {
         eu-west-1 = "ami-0ed7e5f68b9b72a4c"
         eu-west-2 = "ami-04f1828daa77e5c19"
-        eu-north-1 = "ami-844ec6fa"
+        
     }
   
 }
